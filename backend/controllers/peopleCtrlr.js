310a4612be635f10a803feaf66febc67d2aeb4d2
joselinecedano <118120312+joselinecedano@users.noreply.github.com>
2023-03-30 21:56:42 -0400

    can create

--- backend/controllers/peopleCtrlr.js
+++ backend/controllers/peopleCtrlr.js
@@ -1,18 +1,31 @@
 //Require model
-const db = require('../models/People.js')
-console.log(db)
+const db = require("../models");
+console.log(db);
 
-const getPeople = (req,res)=>{
-    //db.People.find({})<--- db has all of our models
-    res.send('this is getPeople!')
+const getPeople = (req, res) => {
+  db.People.find({}) //<--- db has all of our models
+    .then((foundPeople) => {
+      if (!foundPeople) {
+        res.status(404).json({ message: "idk this person" });
+      } else {
+        res.status(200).json({ data: foundPeople });
+      }
+    })
 }
 
-const createPeople = (req,res)=>{
-    res.send('this is createPeople!!')
+const createPeople = (req, res) => {
+  db.People.create(req.body)
+  .then((createdPerson)=>{
+    if(!createdPerson){
+        res.status(404).json({message: 'can`t do it bruv. cannot create person'})
+    }else{
+        res.status(201).json({data: createdPerson})
+    }
+  })
 }
 
-//EXPORT 
-module.exports ={
-    getPeople,
-    createPeople
-}
+//EXPORT
+module.exports = {
+  getPeople,
+  createPeople,
+};
