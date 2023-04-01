adba7bc313f4275e194b7eca23189b5b8048aeff
joselinecedano <118120312+joselinecedano@users.noreply.github.com>
2023-04-01 13:35:21 -0400

    frontend done

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+const Header = (props) => {
+    return (
+        <nav className = 'nav'>
+            <Link to='/'>
+                <div>People App</div>
+            </Link>
+        </nav>
+    )
+}
+
+export default Header;
